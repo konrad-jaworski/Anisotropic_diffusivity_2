792ac55b38e932ec6e1ccd43c4d4e2cbdad03a9b
konrad-jaworski <konrad.agh.imir@gmail.com>
2025-11-20 09:10:26 +0100

    reorg

--- networks.py
+++ networks.py
@@ -8,7 +8,7 @@ class FCN(nn.Module):
     Network used for prediction of thermal diffusivity of the isotropic material with measurments from its surface.
     """
     def __init__(self, layers):
-        super(FCN, self).__init__()
+        super().__init__()
 
         # Layers structure
         self.layers = layers
@@ -76,23 +76,29 @@ class FCN_pseudo_3D(nn.Module):
     And the pseudo
     """
     def __init__(self, layers,branch_layers):
-        super(FCN, self).__init__()
+        super().__init__()
 
         # Layers structure
         self.layers = layers
+        self.branch_layers=branch_layers
 
         # Activation and loss
         self.activation = nn.Tanh()
         self.loss_function = nn.MSELoss(reduction='mean')
 
         # Define the fully connected layers
-        self.linears = nn.ModuleList([
-            nn.Linear(self.layers[i], self.layers[i + 1])
-            for i in range(len(self.layers) - 1)
-        ])
+        self.linears = nn.ModuleList([nn.Linear(self.layers[i], self.layers[i + 1]) for i in range(len(self.layers) - 1)])
+
+        # We define our pseudo branch
+        self.Tzz=nn.ModuleList([nn.Linear(self.branch_layers[i],self.branch_layers[i+1]) for i in range(len(self.branch_layers)-1)])
 
         # Xavier initialization
         for layer in self.linears:
+            nn.init.xavier_normal_(layer.weight)
+            nn.init.zeros_(layer.bias)
+
+        # Xavier initialization for the branch layer
+        for layer in self.Tzz:
             nn.init.xavier_normal_(layer.weight)
             nn.init.zeros_(layer.bias)
 
@@ -103,17 +109,25 @@ class FCN_pseudo_3D(nn.Module):
         # Input shape: [N, 3]  => [t, y, x]
         if not torch.is_tensor(x):
             x = torch.from_numpy(x).float()
+        
+        # Clonning input for input to second branch
+        Tzz_res=x.clone()
 
         for i in range(len(self.linears) - 1):
             x = self.activation(self.linears[i](x))
         x = self.linears[-1](x)
-        return x
+
+        for i in range(len(self.Tzz) - 1):
+            Tzz_res = self.activation(self.Tzz[i](Tzz_res))
+        Tzz_res = self.Tzz[-1](Tzz_res)
+
+        return x,Tzz_res
 
     def PDE_loss(self, x_pde):
         # x_pde: [N, 3] → [t, y, x]
         # Here we are using collocation points which specificic sampling in the domain
         x_pde.requires_grad_(True)
-        u = self(x_pde)
+        u,Tzz_res = self(x_pde)
 
         # Compute first derivatives
         grads = torch.autograd.grad(u, x_pde, torch.ones_like(u), create_graph=True)[0]
@@ -128,14 +142,15 @@ class FCN_pseudo_3D(nn.Module):
         u_xx = grads2_x[:, 2:3]
 
         # PDE residual:
-        f =  u_t - self.a * (u_xx + u_yy)
+        f =  u_t - self.a * (u_xx + u_yy+Tzz_res)
         loss_phys = torch.mean(f ** 2)
 
         return loss_phys
 
     def Data_loss(self, x_data, y_data):
         # Data loss function
-        return self.loss_function(self(x_data), y_data)
+        T_estimate,_=self(x_data)
+        return self.loss_function(T_estimate, y_data)
 
     
 
